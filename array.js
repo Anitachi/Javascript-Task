b14fc4d714ef11283307eabcbbd74fa25a08639c
Anita Chi <101598668+Anitachi@users.noreply.github.com>
2022-06-30 13:36:07 +0100

    Create array.js

---
+++ array.js
@@ -0,0 +1,92 @@
+const names_of = [
+    {names: ["1.", "Thompson", "Anthony", 32 ,"Male",	"Head of Finance Deparment", "17/07/2014"]},
+    {names_1: ["2.", "Johnson", "Mary", 23, "Female", "Software Engineer", "05/03/2019"]},
+    {daysof365: ["3.", "Chisom", "Onyeka", 28,  "Female",	"Human Resource Manager", "21/06/2016"]},
+    {poem: ["4.", "Jide", "Olabisi", "29", "Male", "Assistant Head(Software Department)", "05/06/2016"]},
+    {love: ["5.", "Williams", "Ajayi", "30", "Male", "Head of Software Department", "06/09/2015"]},
+    {prayers:["6.", "Nancy", "Chukwuemeka", "27", "Female", "Secretary", "23/08/2016"]},
+    {kiss: ["7.", "Nicholas", "Uchemba", "26", "Male", "Software Engineer", "22/02/2020"]},
+    {shugzz: ["8.", "Ahmed", "Olayinka", "24", "Male", "Graphic Designer", "15/08/2020"]},
+    {task: ["9.", "Halima", "Mohammed", "26", "Female", "Senior Finance Manager", "30/05/2018"]},
+    {finaldata: ["10.", "Richard", "Adeyemi", "41", "Male", "C.E.O", "17/11/2010"]},
+]
+
+document.getElementById("t1").innerHTML = names_of[0].names[0];
+document.getElementById("t2").innerHTML = names_of[0].names[1];
+document.getElementById("t3").innerHTML = names_of[0].names[2];
+document.getElementById("t4").innerHTML = names_of[0].names[3];
+document.getElementById("t5").innerHTML = names_of[0].names[4];
+document.getElementById("t6").innerHTML = names_of[0].names[5];
+document.getElementById("t7").innerHTML = names_of[0].names[6];
+
+document.getElementById("c1").innerHTML = names_of[1].names_1[0];
+document.getElementById("c2").innerHTML = names_of[1].names_1[1];
+document.getElementById("c3").innerHTML = names_of[1].names_1[2];
+document.getElementById("c4").innerHTML = names_of[1].names_1[3];
+document.getElementById("c5").innerHTML = names_of[1].names_1[4];
+document.getElementById("c6").innerHTML = names_of[1].names_1[5];
+document.getElementById("c7").innerHTML = names_of[1].names_1[6];
+
+document.getElementById("L1").innerHTML = names_of[2].daysof365[0];
+document.getElementById("L2").innerHTML = names_of[2].daysof365[1];
+document.getElementById("L3").innerHTML = names_of[2].daysof365[2];
+document.getElementById("L4").innerHTML = names_of[2].daysof365[3];
+document.getElementById("L5").innerHTML = names_of[2].daysof365[4];
+document.getElementById("L6").innerHTML = names_of[2].daysof365[5];
+document.getElementById("L7").innerHTML = names_of[2].daysof365[6];
+
+document.getElementById("O1").innerHTML = names_of[3].poem[0];
+document.getElementById("O2").innerHTML = names_of[3].poem[1];
+document.getElementById("O3").innerHTML = names_of[3].poem[2];
+document.getElementById("O4").innerHTML = names_of[3].poem[3];
+document.getElementById("O5").innerHTML = names_of[3].poem[4];
+document.getElementById("O6").innerHTML = names_of[3].poem[5];
+document.getElementById("O7").innerHTML = names_of[3].poem[6];
+
+document.getElementById("V1").innerHTML = names_of[4].love[0];
+document.getElementById("V2").innerHTML = names_of[4].love[1];
+document.getElementById("V3").innerHTML = names_of[4].love[2];
+document.getElementById("V4").innerHTML = names_of[4].love[3];
+document.getElementById("V5").innerHTML = names_of[4].love[4];
+document.getElementById("V6").innerHTML = names_of[4].love[5];
+document.getElementById("V7").innerHTML = names_of[4].love[6];
+
+document.getElementById("E1").innerHTML = names_of[5].prayers[0];
+document.getElementById("E2").innerHTML = names_of[5].prayers[1];
+document.getElementById("E3").innerHTML = names_of[5].prayers[2];
+document.getElementById("E4").innerHTML = names_of[5].prayers[3];
+document.getElementById("E5").innerHTML = names_of[5].prayers[4];
+document.getElementById("E6").innerHTML = names_of[5].prayers[5];
+document.getElementById("E7").innerHTML = names_of[5].prayers[6];
+
+document.getElementById("S1").innerHTML = names_of[6].kiss[0];
+document.getElementById("S2").innerHTML = names_of[6].kiss[1];
+document.getElementById("S3").innerHTML = names_of[6].kiss[2];
+document.getElementById("S4").innerHTML = names_of[6].kiss[3];
+document.getElementById("S5").innerHTML = names_of[6].kiss[4];
+document.getElementById("S6").innerHTML = names_of[6].kiss[5];
+document.getElementById("S7").innerHTML = names_of[6].kiss[6];
+
+document.getElementById("a1").innerHTML = names_of[7].shugzz[0];
+document.getElementById("a2").innerHTML = names_of[7].shugzz[1];
+document.getElementById("a3").innerHTML = names_of[7].shugzz[2];
+document.getElementById("a4").innerHTML = names_of[7].shugzz[3];
+document.getElementById("a5").innerHTML = names_of[7].shugzz[4];
+document.getElementById("a6").innerHTML = names_of[7].shugzz[5];
+document.getElementById("a7").innerHTML = names_of[7].shugzz[6];
+
+document.getElementById("p1").innerHTML = names_of[8].task[0];
+document.getElementById("p2").innerHTML = names_of[8].task[1];
+document.getElementById("p3").innerHTML = names_of[8].task[2];
+document.getElementById("p4").innerHTML = names_of[8].task[3];
+document.getElementById("p5").innerHTML = names_of[8].task[4];
+document.getElementById("p6").innerHTML = names_of[8].task[5];
+document.getElementById("p7").innerHTML = names_of[8].task[6];
+
+document.getElementById("i1").innerHTML = names_of[9].finaldata[0];
+document.getElementById("i2").innerHTML = names_of[9].finaldata[1];
+document.getElementById("i3").innerHTML = names_of[9].finaldata[2];
+document.getElementById("i4").innerHTML = names_of[9].finaldata[3];
+document.getElementById("i5").innerHTML = names_of[9].finaldata[4];
+document.getElementById("i6").innerHTML = names_of[9].finaldata[5];
+document.getElementById("i7").innerHTML = names_of[9].finaldata[6];
